4dedb3f87f8c9ab1feeeb090244f179db0d95d22
code-bbob <120273472+code-bbob@users.noreply.github.com>
2024-01-08 16:25:21 +0545

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,8 +1,10 @@
 print("learning pull conflicts")
 
-for x in range(5):
+for x in range(7):
     print(x)
 
-x = int(input("give a value"))
+x = int(input("give some of the a value"))
 if (x>5):
     print('True')
+else:
+    print('False')
